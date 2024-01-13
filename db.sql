cb1cdf9d7af9cdaf1505cf8da9949c8eecfeb914
SirInfinityKing <konto@ratajskipiotr.pl>
2024-01-13 13:32:40 +0100

    dodanie SQLite

---
+++ db.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE mediaexpert (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     kategoria TEXT NOT NULL,
+--     nazwa TEXT NOT NULL,
+--     cena REAL NOT NULL,
+--     data_dodania DATE NOT NULL,
+--     UNIQUE(kategoria, nazwa, data_dodania)
+-- );
+
+-- INSERT INTO mediaexpert (kategoria, nazwa, cena, data_dodania) VALUES ('Pralki', 'Pralka ABC', 1299, '2024-01-13');
+
+-- SELECT * FROM mediaexpert;
+
